78c3e5cd2972dada9a5427b650e6ace3d91cf1a2
ahmedeid <kingmedo954@gmail.com>
2020-09-22 13:09:44 +0200

    completing the cart

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import Product, Category
+from cart.forms import CartAddProduct
 
 
 def product_list(request, category_slug=None):
@@ -23,8 +24,10 @@ def product_list(request, category_slug=None):
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug,
                                 available=True)
+    cart_product_form = CartAddProduct()
     return render(request,
                   'shop/product/detail.html',
                   {
-                      'product': product
+                      'product': product,
+                      'cart_product_form': cart_product_form
                   })

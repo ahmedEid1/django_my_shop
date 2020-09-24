28df101808b33899099af7d5ebb376af9528db9d
ahmedeid <kingmedo954@gmail.com>
2020-09-24 14:33:09 +0200

    redirecting to the payment process after checkout & saving the transaction id

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from .models import OrderItem
 from .forms import OrderForm
@@ -19,7 +20,9 @@ def order_create(request):
                                          quantity=item['quantity'])
             cart.clear()
             order_created.delay(order.id)
-            return render(request, 'orders/order/created.html')
+
+            request.session['order_id'] = order.id
+            return redirect(reverse('payment:process'))
     else:
         form = OrderForm()
 
